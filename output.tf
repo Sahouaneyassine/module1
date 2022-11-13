bdf41471561f3a9c33535400427dbb84f5e29f5e
Your Name <you@example.com>
2022-11-13 14:07:13 +0100

    last

--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
-output "person3" {
-  description = "Person list converted"
+output "list_converted" {
+  description = "The Person detail list converted : "
   value       =  tolist([element(var.person, 0),element(var.person, 2)])
 }
--- variables.tf
+++ variables.tf
@@ -1,12 +1,12 @@
 variable "person" {
   type        = list(string)
-  description = "Details of the person (Prenom,Nom,Num) ? "
+  description = "Details of the person (Prenom,Nom,Phone Num) ? "
   validation {
     condition     = length(var.person) == 3
-    error_message = "The list must contains 3 elements (Prenom , Non , Num)"
+    error_message = "The list must contains 3 elements (Prenom , Non ,Phone Num)"
   }
   validation {
-    error_message = "Value Must  contain only numbers."
-    condition = can( regex("^[0-9]+$",element(var.person, 2)) )
+    condition = can(regex("^[0-9]+$",element(var.person, 2)))
+    error_message = "THe phone number must contain only numbers. Ex:(06534979)"
   }
 }
